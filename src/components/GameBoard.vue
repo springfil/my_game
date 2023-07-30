314660e670673d67f9921a832e55e47290686bd5
springfil <st.kenzhaev@gmail.com>
2023-07-30 22:11:36 +0300

    восстановление

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,18 +1,16 @@
 <script setup>
-<<<<<<< HEAD
-import { ref,inject } from "vue";
-import BoardItem from "./BoardItem";
-import useGameInit from "../composable/useGameInit";
-import useGameStart from "../composable/useGameStart";
-import useGameProcess from "../composable/useGameProcess";
-import { GAME_STATUS } from "../constants/GAME_STATUS";
+import { ref, inject } from "vue";
+import BoardItem from "@/components/BoardItem.vue";
+import useGameInit from "@/composable/useGameInit";
+import useGameStart from "@/composable/useGameStart";
+import useGameProcess from "@/composable/useGameProcess";
+import { GAME_STATUS } from "@/constants/GAME_STATUS";
 
-
-const data = inject('data');
+const data = inject("data");
 
 const updateData = () => {
-    data.value = difficult.value;
-}; 
+  data.value = difficult.value;
+};
 
 const numberOfCells = 25;
 const gameStatus = ref(GAME_STATUS.NONE);
@@ -34,42 +32,9 @@ const { selectField, isNext, isReset } = useGameProcess(
   difficult,
   start
 );
-=======
-import BoardItem from "./BoardItem";
-import { ref, onBeforeMount } from "vue";
-
-const difficult = ref('Легкая');
-let fields = ref([]);
-const number = 36;
-
-const init = () => {
-  fields.value = [];
-
-  for (let i = 0; i < number; i++){
-    fields.value.push({
-      id: i,
-      clicked: false,
-      value: 0
-    });
-  }
-};
-
-onBeforeMount(init);
-
-const start = () => {
-  init();
-  prepareGame();
-};
-
-const prepareGame = () => {
-
-};
->>>>>>> 96ed41a (create gameboard with some logic)
-
 </script>
 
 <template>
-<<<<<<< HEAD
   <div class="test">
     <div class="board-wrapper">
       <div class="board">
@@ -95,19 +60,6 @@ const prepareGame = () => {
   </div>
 </template>
 
-=======
-    <div class="board-wrapper">
-        <div class="board">
-          <board-item v-for="field in fields" :key="'item-' + field.id" />
-        </div>
-        
-        <p class="difficult"> Сложность : <strong>{{ difficult }}</strong></p>
-        <button class="btn" @click="start"> Старт </button>
-    </div>
-</template>
-
-
->>>>>>> 96ed41a (create gameboard with some logic)
 <style scoped>
 .board-wrapper {
   display: flex;
@@ -116,7 +68,6 @@ const prepareGame = () => {
   justify-content: center;
 }
 .board {
-<<<<<<< HEAD
   width: 350px;
   background: #eee;
   border: none;
@@ -143,26 +94,5 @@ const prepareGame = () => {
   align-items: center;
   justify-content: center;
   margin-top: 10px;
-=======
-  width: 360px;
-  background: #eee;
-  border: none;
-  border-radius: 10px ;
-}
-
-.btn{
-    background: #2d3741;
-    color: white;
-    border: none;
-    border-radius: 10px ;
-    padding: 10px 30px;
-    margin: 10px 0;
-    cursor: pointer;
-    outline: none;
-}
-
-.btn:hover {
-    background: #c0392b;
->>>>>>> 96ed41a (create gameboard with some logic)
 }
 </style>
